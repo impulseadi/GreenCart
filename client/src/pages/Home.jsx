e7eca611a4fa106a0edae7c3b96de9f84499baec
Aditya <adigupta.951@gmail.com>
2025-05-13 16:54:32 +0530

    git

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,23 +1,27 @@
-import React from "react"
-import BestSeller from "../components/BestSeller"
-import BottomBanner from "../components/BottomBanner"
+import React, { Suspense } from "react";
+import BestSeller from "../components/BestSeller";
+import BottomBanner from "../components/BottomBanner";
+import MainBanner from "../components/MainBanner";
+import NewsLetter from "../components/NewsLetter";
 
-import MainBanner from "../components/MainBanner"
-import NewsLetter from "../components/NewsLetter"
-
+// Lazy load the Categories component
 const Categories = React.lazy(() => import("../components/Categories"));
+
 const Home = () => {
   return (
     <div className="mt-10">
         <MainBanner />
+        
+        {/* Suspense to handle loading state of Categories */}
         <Suspense fallback={<div>Loading...</div>}>
-        <Categories />
-      </Suspense>
+            <Categories />
+        </Suspense>
+        
         <BestSeller />
         <BottomBanner />
-        <NewsLetter/>
+        <NewsLetter />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
